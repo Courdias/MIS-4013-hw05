2525119982bfebb2e01e97cadf07cc816e4af16a
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 13:56:21 -0600

    Update function4.php

--- function4.php
+++ function4.php
@@ -6,17 +6,7 @@
       <button class="btn" onclick='PrintProduct()'>Multiply 10*2</button>
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
-<div class="container">
-      <p id="RNG"></p>
-      <button class="btn" onclick='RandomNum()'>Guess a Number 1-9</button>
-</div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
         <script>
-                function RandomNum() {
-                var a = Math.floor(Math.random() * 10)
-                return document.getElementById('RNG').innerHTML = a;
-</script>
-<script>
                 function Multiply(a, b) {
                           return a * b;
                 }
