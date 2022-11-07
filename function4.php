76af410613db6e3cb98dfa65bbafaa65b93b0b12
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 12:14:19 -0600

    Update function4.php

--- function4.php
+++ function4.php
@@ -2,13 +2,13 @@
   include "header.php";
 ?>
     <div class="container">
-      <p id="function4"></p>
+      <p id="math"></p>
       <button class="btn" onclick='myFunction()'>Multiply</button>
     </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
 <script>
 var x = myFunction(4, 3);
-document.getElementById("demo").innerHTML = x;
+document.getElementById("math").innerHTML = x;
 
 function myFunction(a, b) {
   return a * b;
