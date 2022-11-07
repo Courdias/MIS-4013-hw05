dd72375f9ed058671ae02d018cb4c79e238e44f6
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 12:53:34 -0600

    Update function5.php

--- function5.php
+++ function5.php
@@ -3,14 +3,14 @@
 ?>
     <div class="container">
       <p id="spain"></p>
-      <button class="btn" onclick='getImagePath()'>Surprise</button>
+      <button class="btn" onclick='PrintImage()'>Surprise</button>
     </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
 <script>
-  function getImagePath() {
-  return "https://www.ru.nl/publish/pages/947657/flying_circus_meme.jpg";
-}
 
+function PrintImage() {
+        document.getElementById('spain').innerHTML = "https://www.ru.nl/publish/pages/947657/flying_circus_meme.jpg";
+      }
           </script>
         <img src onerror="this.onerror=null; this.src=getImagePath();">  
 
