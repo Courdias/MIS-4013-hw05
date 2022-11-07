82a8bce9ad643a8249e28ca7ce7be9efd8acec4f
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 12:34:59 -0600

    Update function3.php

--- function3.php
+++ function3.php
@@ -8,8 +8,8 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
 <script>
   function RandomNum() {
-        document.getElementById('RNG').innerHTML = Math.floor(Math.random() * 10);
-    return "Did you guess correctly?";
+      return document.getElementById('RNG').innerHTML = Math.floor(Math.random() * 10) + "Did you guess correctly?";
+     
   }
         </script>
 <div>Did you guess correctly?</div>
