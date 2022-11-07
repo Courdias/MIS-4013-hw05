f446d66e0be969f6641de26abb4a79b08264be1a
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 12:59:42 -0600

    Update function5.php

--- function5.php
+++ function5.php
@@ -2,21 +2,22 @@
   include "header.php";
 ?>
     <div class="container">
+      <img id="image" src="https://www.ru.nl/publish/pages/947657/flying_circus_meme.jpg" alt="Spain image"/>
       <p id="spain"></p>
       <button class="btn" onclick='PrintImage()'>Surprise</button>
     </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
 <script>
 
-function PrintImage() {
-        document.getElementById('spain').innerHTML = Image();
-      }
-  
-function Image(){
-    return "https://www.ru.nl/publish/pages/947657/flying_circus_meme.jpg";
-  }
-          </script>
-        <img src onerror="this.onerror=null; this.src=getImagePath();">  
-
+        function PrintImage() {
+ 
+            /* Access image by id and change
+            the display property to block*/
+            document.getElementById('image')
+                    .style.display = "block";
+ 
+            document.getElementById('spain')
+                    .style.display = "none";
+        }
     </body>
 </html>
