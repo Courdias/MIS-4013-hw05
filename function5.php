372a1e6774351c9549c4a8fa9a79a47c67c05be8
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 13:06:04 -0600

    Update function5.php

--- function5.php
+++ function5.php
@@ -4,8 +4,12 @@
 
 	<div>
 		<img id="image" src="" />
-		      <button class="btn" onclick='Show()' id=btnID>Surprise</button>
 	</div>
+<div class="container">
+	<p id="placeholder"></p>
+	<button class="btn" onclick='Show()' id=btnID>Surprise</button>
+</div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
 	<script>
