23762d5338591e666cc46bdbfe52f3a9a70dfc1f
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 12:13:32 -0600

    Update function2.php

--- function2.php
+++ function2.php
@@ -3,12 +3,12 @@
 ?>
     <div class="container">
       <p id="date"></p>
-      <button class="btn" onclick='writeValue()'>Show the Time</button>
+      <button class="btn" onclick='giveDate()'>Show the Time</button>
     </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
 <script>
   const d = new Date();
-  function writeValue() {
+  function giveDate() {
         document.getElementById('date').innerHTML = d;
       }
         </script>
