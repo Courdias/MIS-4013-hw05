15f9dae5ac63f12fc3369a2ae0516aa5d352668b
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 13:46:49 -0600

    Update function5.php

--- function5.php
+++ function5.php
@@ -1,29 +1,20 @@
 <?php
   include "header.php";
 ?>
-
-	<div>
-		<img id="image" src="" />
-	</div>
+<div>
+	<img id="image" src="" />
+</div>
 <div class="container">
 	<p id="placeholder"></p>
 	<button class="btn" onclick='show()' id=btnID>Surprise</button>
 </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
 	<script>
 		function show() {
-
-			/* Get image and change value
-			of src attribute */
 			let image = document.getElementById("image");
-
-			image.src =
-"https://www.ru.nl/publish/pages/947657/flying_circus_meme.jpg"
-      
-			document.getElementById("btnID")
-					.style.display = "none";
+			image.src = "https://www.ru.nl/publish/pages/947657/flying_circus_meme.jpg"
+			
+			document.getElementById("btnID").style.display = "none";
 		}
 	</script>
     </body>
