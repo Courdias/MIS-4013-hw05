ac37eaa6494af014f87c6e60d6d1bd18a7eefe7c
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 13:02:06 -0600

    Update function5.php

--- function5.php
+++ function5.php
@@ -1,23 +1,34 @@
 <?php
   include "header.php";
 ?>
-    <div class="container">
-      <img id="image" src="https://www.ru.nl/publish/pages/947657/flying_circus_meme.jpg" alt="Spain image"/>
-      <p id="spain"></p>
-      <button class="btn" onclick='PrintImage()'>Surprise</button>
-    </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
-<script>
 
-        function PrintImage() {
- 
-            /* Access image by id and change
-            the display property to block*/
-            document.getElementById('image')
-                    .style.display = "block";
- 
-            document.getElementById('spain')
-                    .style.display = "none";
-        }
+	<div>
+		<h1>GeeksforGeeks</h1>
+		<h3>Click on the button to see image</h3>
+
+		<!-- img element without src attribute -->
+		<img id="image" src="" />
+	</div>
+
+	<button type="button"
+		onclick="show()" id="btnID">
+		Show Image
+	</button>
+
+	<script>
+		function show() {
+
+			/* Get image and change value
+			of src attribute */
+			let image = document.getElementById("image");
+
+			image.src =
+"https://www.ru.nl/publish/pages/947657/flying_circus_meme.jpg"
+      
+			document.getElementById("btnID")
+					.style.display = "none";
+		}
+	</script>
+
     </body>
 </html>
