ed56cc6d8c0041177c1d54a05876e67810594466
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 12:51:41 -0600

    Update function5.php

--- function5.php
+++ function5.php
@@ -3,7 +3,7 @@
 ?>
     <div class="container">
       <p id="spain"></p>
-      <button class="btn" onclick='PrintImage()'>Surprise</button>
+      <button class="btn" onclick='getImagePath()'>Surprise</button>
     </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
 <script>
@@ -12,9 +12,6 @@
 }
         <img src onerror="this.onerror=null; this.src=getImagePath();">  
 
-function PrintImage() {
-        document.getElementById('spain').innerHTML = getImagePath();
-}
         </script>
     </body>
 </html>
