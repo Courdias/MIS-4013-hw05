382199dc7e8c1257d872991d6f4077106f0fad8f
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 12:46:14 -0600

    Update function5.php

--- function5.php
+++ function5.php
@@ -3,17 +3,18 @@
 ?>
     <div class="container">
       <p id="Spain"></p>
-      <button class="btn" onclick='getImagePath()'>Surprise</button>
+      <button class="btn" onclick='PrintImage()'>Surprise</button>
     </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
 <script>
   function getImagePath() {
   return "https://www.ru.nl/publish/pages/947657/flying_circus_meme.jpg";
 }
-
-      </script> 
-      <!-- doesn't fire error event in FireFox --> 
       <img src onerror="this.onerror=null; this.src=getImagePath();">  
+
+function PrintImage(){
+        document.getElementById('placeholder').innerHTML = getImagePath();
+}
         </script>
     </body>
 </html>
