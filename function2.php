50bd1424e4a77cce9f05bd5d17d210e45dd8e712
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 11:52:17 -0600

    Update function2.php

--- function2.php
+++ function2.php
@@ -5,11 +5,10 @@
       <p id="placeholder"></p>
       <button class="btn" onclick='writeValue()'>Show the Time</button>
     </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-<p id="demo"></p>    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
 <script>
         const d = new Date();
-        document.getElementById("demo").innerHTML = d;
+        document.getElementById("placeholder").innerHTML = d;
         </script>
     </body>
 </html>
