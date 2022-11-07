db0a7076fd25ab23196feafd45d618e3cf4f86bf
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 12:12:16 -0600

    Update function3.php

--- function3.php
+++ function3.php
@@ -3,7 +3,7 @@
 ?>
     <div class="container">
       <p id="RNG"></p>
-      <button class="btn" onclick='RandNum()'>Guess a Number 1-9</button>
+      <button class="btn" onclick='RandomNum()'>Guess a Number 1-9</button>
     </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
 <script>
