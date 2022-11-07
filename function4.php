4569e4a92b45beaae67184c64e0226a7febe9796
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 12:11:30 -0600

    Create function4.php

---
+++ function4.php
@@ -0,0 +1,18 @@
+<?php
+  include "header.php";
+?>
+    <div class="container">
+      <p id="function4"></p>
+      <button class="btn" onclick='myFunction()'>Multiply</button>
+    </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
+<script>
+var x = myFunction(4, 3);
+document.getElementById("demo").innerHTML = x;
+
+function myFunction(a, b) {
+  return a * b;
+}
+        </script>
+    </body>
+</html>
