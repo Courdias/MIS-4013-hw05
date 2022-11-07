99e5fa6a0670af85c0912953188a4159663b8918
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 13:48:25 -0600

    Update function4.php

--- function4.php
+++ function4.php
@@ -1,18 +1,18 @@
 <?php
   include "header.php";
 ?>
-    <div class="container">
+<div class="container">
       <p id="math"></p>
       <button class="btn" onclick='PrintProduct()'>Multiply 10*2</button>
-    </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
-<script>
-function Multiply(a, b) {
-  return a * b;
-}
-  function PrintProduct(){
-document.getElementById("math").innerHTML = Multiply(10, 2); //edit here to change numbers being multiplied
-  }
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
+        <script>
+                function Multiply(a, b) {
+                          return a * b;
+                }
+                function PrintProduct(){
+                          document.getElementById("math").innerHTML = Multiply(10, 2); //edit here to change numbers being multiplied
+                }
         </script>
     </body>
 </html>
