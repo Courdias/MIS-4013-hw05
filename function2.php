a9ce2ad058854309e4a839bcf6d5fda0bf695d82
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 11:55:03 -0600

    Update function2.php

--- function2.php
+++ function2.php
@@ -2,13 +2,15 @@
   include "header.php";
 ?>
     <div class="container">
-      <p id="placeholder"></p>
+      <p id="date"></p>
       <button class="btn" onclick='writeValue()'>Show the Time</button>
     </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
 <script>
-        const d = new Date();
-        document.getElementById("placeholder").innerHTML = d;
+  const d = new Date();
+  function writeValue() {
+        document.getElementById('date').innerHTML = 'd';
+      }
         </script>
     </body>
 </html>
