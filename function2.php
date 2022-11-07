44c27ecf7ae61c18ddd10e3e8136552b98fa25f3
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 13:41:34 -0600

    Update function2.php

--- function2.php
+++ function2.php
@@ -7,9 +7,10 @@
     </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
 <script>
-  var d = new Date();
+
   function giveDate() {
-        document.getElementById('date').innerHTML = d;
+      const d = new Date();
+      document.getElementById('date').innerHTML = d;
       }
         </script>
     </body>
