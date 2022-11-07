94d27d02dcdf5ef5532d0d621635d4c7df615c3a
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 12:05:25 -0600

    Update function3.php

--- function3.php
+++ function3.php
@@ -8,7 +8,10 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
 <script>
   function writeValue() {
-        document.getElementById('RNG').innerHTML = Math.floor(Math.random() * 10) <br/> 'Did you guess correctly?';
+        document.getElementById('RNG').innerHTML = Math.floor(Math.random() * 10);
+  }
+  function writeValue() {
+        document.getElementById('RNG').innerHTML ='Did you guess correctly?';
   }
         </script>
     </body>
