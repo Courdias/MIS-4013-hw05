3755e1abbfd655c07849faf01e85e4d5b188d536
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 12:00:29 -0600

    Update function3.php

--- function3.php
+++ function3.php
@@ -3,15 +3,14 @@
 ?>
     <div class="RNG">
       <p id="date"></p>
-      <button class="btn" onclick='writeValue()'>Show the Time</button>
+      <button class="btn" onclick='writeValue()'>Guess a Number 1-9</button>
     </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
 <script>
-document.getElementById("RNG").innerHTML =
-
   function writeValue() {
-        document.getElementById('date').innerHTML = Math.floor(Math.random() * 10);;
+        document.getElementById('RNG').innerHTML = Math.floor(Math.random() * 10);
       }
         </script>
+<div> Did you guess correctly?</div>
     </body>
 </html>
