7f195e259f9fbe49bf74238fdbd3e9b8928ca57d
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 11:50:35 -0600

    Update function2.php

--- function2.php
+++ function2.php
@@ -6,7 +6,8 @@
       <button class="btn" onclick='writeValue()'>Click me</button>
     </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <script>
+<p id="demo"></p>    
+<script>
         const d = new Date();
         document.getElementById("demo").innerHTML = d;
         </script>
