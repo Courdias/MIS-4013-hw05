773b6da4957e6d22c669a4495a6116c4629c34ad
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 13:58:40 -0600

    Update function3.php

--- function3.php
+++ function3.php
@@ -1,16 +1,15 @@
 <?php
   include "header.php";
 ?>
-    <div class="container">
+<div class="container">
       <p id="RNG"></p>
       <button class="btn" onclick='RandomNum()'>Guess a Number 1-9</button>
-    </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
-<script>
-  function RandomNum() {
-      return document.getElementById('RNG').innerHTML = Math.floor(Math.random() * 10) + "\nDid you guess correctly?";
-     
-  }
+</div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
+        <script>
+                function RandomNum() {
+                return document.getElementById('RNG').innerHTML = Math.floor(Math.random() * 10) + "\nDid you guess correctly?";
+        }
         </script>
     </body>
 </html>
