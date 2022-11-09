71899cae41a90ccdd7cc907f1b5d50560c1a7140
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-09 13:29:32 -0600

    Update function4.php

--- function4.php
+++ function4.php
@@ -17,9 +17,10 @@
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
         <script>
-          var a = parseInt(document.getElementById('a').value);
-          var b = parseInt(document.getElementById('b').value);
+
                 function Multiply() {
+                          var a = parseInt(document.getElementById('a').value);
+                          var b = parseInt(document.getElementById('b').value);
                           document.getElementById('math').value = a * b;
                 }
             
