615c3d9ffc7629836f434c8ff3874ef54200ec13
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-09 13:02:14 -0600

    Update function4.php

--- function4.php
+++ function4.php
@@ -3,11 +3,11 @@
 ?>
 <form>
                 <label>Number 1: </label>
-                <input type="text" name="n1" placeholder="Number 1 here"/>
+                <input type="text" name="a" placeholder="Number 1 here"/>
                 <br/>
 
                 <label>Number 2:  </label>
-                <input type="text" name="n2" placeholder="Number 2 here" />
+                <input type="text" name="b" placeholder="Number 2 here" />
                 <br/>
 </form>
 
@@ -17,13 +17,13 @@
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
         <script>
-          var n1 = document.getElementById('n1').value;
-          var n2 = document.getElementById('n2').value;
+          var a = document.getElementById('a').value;
+          var b = document.getElementById('b').value;
                 function Multiply(a, b) {
                           return a * b;
                 }
                 function PrintProduct(){
-                          document.getElementById("math").innerHTML = Multiply(n1, n2); //edit here to change numbers being multiplied
+                          document.getElementById("math").innerHTML = Multiply(a, b); //edit here to change numbers being multiplied
                 }
         </script>
     </body>
