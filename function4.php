ea8840a63e364defdfd677910ba236c417227133
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-09 13:22:57 -0600

    Update function4.php

--- function4.php
+++ function4.php
@@ -13,7 +13,7 @@
 
 <div class="container">
       <p id="math"></p>
-      <button class="btn" onclick="PrintProduct();">Multiply</button>
+      <button class="btn" onclick="Multiply();">Multiply</button>
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
         <script>
@@ -21,9 +21,7 @@
           var b = parseInt(document.getElementById('b').value);
                 function Multiply() {
                           document.getElementById("math").value = a * b;
-                }
-                function PrintProduct(){
-                          return document.getElementById('math').innerHTML =  Multiply();
+                          return document.getElementById('math').innerHTML
                 }
             
         </script>
